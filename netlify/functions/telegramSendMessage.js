2ef55c3ecbf8156d02424a7fb87809694d03b637
DillaFashion <dillafashion887@gmail.com>
2025-10-26 08:10:33 +0530

    Update telegramSendMessage.js

--- netlify/functions/telegramSendMessage.js
+++ netlify/functions/telegramSendMessage.js
@@ -5,9 +5,9 @@ const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 export default async (req, res) => {
   const { chatId, text } = JSON.parse(req.body);
   await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ chat_id: chatId, text }),
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body: JSON.stringify({ chat_id: chatId, text })
   });
-  res.status(200).json({ status: "ok" });
+  res.status(200).json({status:"ok"});
 };
